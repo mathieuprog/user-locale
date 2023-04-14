22a691345aef66645e36e251cea1ecb257dcba43
mathieuprog <5883963+mathieuprog@users.noreply.github.com>
2023-04-14 08:47:21 +0200

    Add NumberFormat enum

---
+++ src/NumberFormat.ts
@@ -0,0 +1,7 @@
+export enum NumberFormat {
+  CommaPeriod = 'COMMA_PERIOD', // 1,234.56
+  PeriodComma = 'PERIOD_COMMA', // 1.234,56
+  SpaceComma = 'SPACE_COMMA', // 1 234,56
+}
+
+export default NumberFormat;
